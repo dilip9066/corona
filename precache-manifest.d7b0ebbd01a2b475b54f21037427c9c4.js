61095a3175e34ac2648da5ce85cbb9fe0363e0d1
dilip <dilip.shital@gmail.com>
2020-07-14 13:42:11 +0530

    Updates

---
+++ precache-manifest.d7b0ebbd01a2b475b54f21037427c9c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0425aea3ba2d0fff76d730c450abec47",
+    "url": "/corona/index.html"
+  },
+  {
+    "revision": "6200e1cd8b815a1f0caf",
+    "url": "/corona/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "926ac0cad321f222793f",
+    "url": "/corona/static/js/2.f7035d65.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/corona/static/js/2.f7035d65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6200e1cd8b815a1f0caf",
+    "url": "/corona/static/js/main.24afdf36.chunk.js"
+  },
+  {
+    "revision": "9a0afa329afbe2d64b67",
+    "url": "/corona/static/js/runtime-main.94a2f559.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/corona/static/media/logo.5d5d9eef.svg"
+  }
+]);
